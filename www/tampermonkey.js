e66c744d24c731a8144bae8db8c3d06fcb936628
Hugues Vogel <Hugues Vogel>
2017-04-03 15:29:30 +0200

    allup2

--- www/tampermonkey.js
+++ www/tampermonkey.js
@@ -106,8 +106,8 @@ test2 = null;
             console.log(js_q_data[i].title+" == "+titre+" => "+(js_q_data[i].title == titre?"true":"false"));
             for(var i2=0;i2<js_q_data[i].question.length;i2++){
             	$(".qtext").each(function(k,q){
-            		console.debug($(q).text() +"=="+ js_q_data[i].question[i2].text)
-        	    	if($(q).text() == js_q_data[i].question[i2].text){
+            		//console.debug($(q).text() +"=="+ js_q_data[i].question[i2].text)
+        	    	if($(q).html() == js_q_data[i].question[i2].text){
                         var rdiv_ = $("<div></div>")
 	                        .text(js_q_data[i].question[i2].rep)
 	                        .addClass("formulation")
